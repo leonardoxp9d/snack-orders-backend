d332542072d6f1bcbe0c1c9f170bd9f260ac8db4
Leonardo Soares <leonardoxp9d@gmail.com>
2023-06-26 14:18:51 -0300

    error sse

--- src/controllers/order/ListOrdersController.ts
+++ src/controllers/order/ListOrdersController.ts
@@ -5,9 +5,23 @@ class ListOrdersController {
     async handle( request: Request, response: Response) {
         const listOrderService = new ListOrdersService();
         
+        response.writeHead(200, {
+            'Content-Type': 'text/event-stream',
+            'Cache-Control': 'no-cache',
+            'connection': 'keep-alive',
+        });
+
+        setInterval( async () => {
+            const orders = await listOrderService.execute();
+            console.log('sent:', orders);
+            response.write(`data: ${JSON.stringify(orders)}\n\n`);
+        }, 1000);
+
+
+        /*
         const orders = await listOrderService.execute();
 
-        return response.json(orders);
+        return response.json(orders);*/
     }
 }
 
